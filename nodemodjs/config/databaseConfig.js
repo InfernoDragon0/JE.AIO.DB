19c01c50863d55cf0f3d1988d1e4b07bd88eec7a
Unknown <calebcheongdnt@gmail.com>
2017-09-28 16:15:31 +0800

    push

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -1,6 +1,6 @@
 var databaseConfig = {}
 
-databaseConfig.url = 'http://9919580f.ngrok.io/api'
+databaseConfig.url = 'http://johorbahru.azurewebsites.net/api'
 
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
